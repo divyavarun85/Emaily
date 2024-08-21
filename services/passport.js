abda3f0fc2f711996bf5e21ea592c6d677b8e4a2
Divya <divyavarun85@gmail.com>
2024-08-21 13:22:13 -0400

    added prod keys

---
+++ services/passport.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys')
+
+const User = mongoose.model('users') // one argument means we are fetching sonething and more than one arg means we are loading something to mongoose model
+
+  passport.serializeUser((user,done)=>{
+    done(null, user.id);
+  });
+
+passport.deserializeUser((id,done) =>{
+User.findById(id)
+.then((user) =>{
+  done(null,user);
+})
+})
+
+
+passport.use( new GoogleStrategy({
+  clientID:keys.googleClientID,
+  clientSecret:keys.googleClientSecret,
+  callbackURL:'/auth/google/callback',
+  proxy:true
+},(accessToken,refreshToken,profile,done)=>{
+  User.findOne({googleId:profile.id})
+  .then((existingUser) =>{
+    if(existingUser){
+      //we already have a record with same id
+      console.log('existingUser',existingUser);
+  done(null,existingUser);
+    }else{
+      //we document, make a new record
+      new User({googleId:profile.id})
+      .save().    //create a new modal instance 
+      then ((user)=> done(null,user));
+    }
+  } )
+ 
+
+}));
