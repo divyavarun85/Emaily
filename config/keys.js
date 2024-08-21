02cc8c6e2d4f54ef0a6eacc5ff782c28835e8e19
Divya <divyavarun85@gmail.com>
2024-08-21 14:04:12 -0400

    prod validation, create react app

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
+  console.log('in production');
+ console.log(require('./prod')) ;
 module.exports = require('./prod');
 }else{
   module.exports = require('./dev');
